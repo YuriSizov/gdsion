d0c430e8a8d52a8ee42a6bac4120d2f19ee55e6e
Yuri Sizov <yuris@humnom.net>
2024-04-30 20:50:42 +0200

    CI: Switch to a custom zip action
    This allows us to customize it to our needs, such as
    zipping subfolders into separate archives.
    The new actions is also simpler as unused features
    of the original are stripped.

---
+++ .github/actions/zip-folder/zip.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+# Adapted from https://github.com/TheDoctor0/zip-release.
+
+# Create archive or exit if the command fails
+set -eu
+printf "\n📦 Creating zip archive...\n"
+
+if [ "$RUNNER_OS" = "Windows" ]; then
+    if [ "$ARCHIVE_SPLIT" = 1 ]; then
+        for dir in $ARCHIVE_INCLUDE_PATH/*/ ; do
+            7z a -tzip "$dir" $ARCHIVE_INCLUDE_PATH/$dir/ || { printf "\n⛔ Unable to create zip archive.\n"; exit 1; }
+            printf "\n✔ Successfully created %s archive.\n" "$dir"
+        done
+    else
+        7z a -tzip "$ARCHIVE_OUTPUT_NAME" $ARCHIVE_INCLUDE_PATH || { printf "\n⛔ Unable to create zip archive.\n"; exit 1; }
+        printf "\n✔ Successfully created %s archive.\n" "$ARCHIVE_OUTPUT_NAME"
+    fi
+else
+    if [ "$ARCHIVE_SPLIT" = 1 ]; then
+        for dir in $ARCHIVE_INCLUDE_PATH/*/ ; do
+            zip -r "$dir" $ARCHIVE_INCLUDE_PATH/$dir/ || { printf "\n⛔ Unable to create zip archive.\n"; exit 1; }
+            printf "\n✔ Successfully created %s archive.\n" "$dir"
+        done
+    else
+        zip -r "$ARCHIVE_OUTPUT_NAME" $ARCHIVE_INCLUDE_PATH || { printf "\n⛔ Unable to create zip archive.\n"; exit 1; }
+        printf "\n✔ Successfully created %s archive.\n" "$ARCHIVE_OUTPUT_NAME"
+    fi
+fi
+
