6fec3194d72820740e41aa972494c6ce778d8a40
Yuri Sizov <yuris@humnom.net>
2024-04-30 21:17:52 +0200

    CI: Fix various issues with the new zip action

--- .github/actions/zip-folder/zip.sh
+++ .github/actions/zip-folder/zip.sh
@@ -8,22 +8,28 @@ printf "\n📦 Creating zip archive...\n"
 
 if [ "$RUNNER_OS" = "Windows" ]; then
     if [ "$ARCHIVE_SPLIT" = 1 ]; then
-        for dir in $ARCHIVE_INCLUDE_PATH/*/ ; do
-            7z a -tzip "$dir" $ARCHIVE_INCLUDE_PATH/$dir/ || { printf "\n⛔ Unable to create zip archive.\n"; exit 1; }
-            printf "\n✔ Successfully created %s archive.\n" "$dir"
+        for name in $ARCHIVE_INCLUDE_PATH/*; do
+            if [ -d "$name" ]; then
+                include_path="$ARCHIVE_INCLUDE_PATH/$name/"
+                7z a -tzip "$name.zip" $include_path || { printf "\n⛔ Unable to create zip archive from %s.\n" "$include_path"; exit 1; }
+                printf "\n✔ Successfully created %s archive.\n" "$name.zip"
+            fi
         done
     else
-        7z a -tzip "$ARCHIVE_OUTPUT_NAME" $ARCHIVE_INCLUDE_PATH || { printf "\n⛔ Unable to create zip archive.\n"; exit 1; }
+        7z a -tzip "$ARCHIVE_OUTPUT_NAME" $ARCHIVE_INCLUDE_PATH || { printf "\n⛔ Unable to create zip archive from %s.\n" "$ARCHIVE_INCLUDE_PATH"; exit 1; }
         printf "\n✔ Successfully created %s archive.\n" "$ARCHIVE_OUTPUT_NAME"
     fi
 else
     if [ "$ARCHIVE_SPLIT" = 1 ]; then
-        for dir in $ARCHIVE_INCLUDE_PATH/*/ ; do
-            zip -r "$dir" $ARCHIVE_INCLUDE_PATH/$dir/ || { printf "\n⛔ Unable to create zip archive.\n"; exit 1; }
-            printf "\n✔ Successfully created %s archive.\n" "$dir"
+        for name in $ARCHIVE_INCLUDE_PATH/*; do
+            if [ -d "$name" ]; then
+                include_path="$ARCHIVE_INCLUDE_PATH/$name/"
+                zip -r "$name.zip" $include_path || { printf "\n⛔ Unable to create zip archive from %s.\n" "$include_path"; exit 1; }
+                printf "\n✔ Successfully created %s archive.\n" "$name.zip"
+            fi
         done
     else
-        zip -r "$ARCHIVE_OUTPUT_NAME" $ARCHIVE_INCLUDE_PATH || { printf "\n⛔ Unable to create zip archive.\n"; exit 1; }
+        zip -r "$ARCHIVE_OUTPUT_NAME" $ARCHIVE_INCLUDE_PATH || { printf "\n⛔ Unable to create zip archive from %s.\n" "$ARCHIVE_INCLUDE_PATH"; exit 1; }
         printf "\n✔ Successfully created %s archive.\n" "$ARCHIVE_OUTPUT_NAME"
     fi
 fi
