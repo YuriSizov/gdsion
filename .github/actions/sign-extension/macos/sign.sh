d36110e32b9cc6e9f93d44d1c3f804c2ca85341d
Yuri Sizov <yuris@humnom.net>
2024-06-04 18:01:31 +0200

    CI: Add a codesign step for macOS extension builds
    This is not fully tested yet because of Apple issues.
    The notarization process can hang in perpetuity, with
    no way for the developer to affect it or even to
    understand the source of the hiccup.
    
    But the overall flow for the extension codesign should
    be correct here. We still need to do the same for the
    example project, but let's see if we can get this working first.

---
+++ .github/actions/sign-extension/macos/sign.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Based on https://github.com/godot-jolt/godot-jolt/blob/master/scripts/ci_sign_macos.ps1
+
+apple_dev_id="$APPLE_DEV_ID"
+apple_dev_app_id="$APPLE_DEV_APP_ID"
+apple_dev_team_id="$APPLE_DEV_TEAM_ID"
+apple_dev_password="$APPLE_DEV_PASSWORD"
+
+framework_path="$FRAMEWORK_PATH"
+archive_path="$ARCHIVE_PATH"
+
+if [ -z "${apple_dev_id}" ]; then
+  echo "ERROR: Missing Apple developer ID."
+  exit 1
+fi
+if [ -z "${apple_dev_app_id}" ]; then
+  echo "ERROR: Missing Apple developer application ID."
+  exit 1
+fi
+if [ -z "${apple_dev_team_id}" ]; then
+  echo "ERROR: Missing Apple team ID."
+  exit 1
+fi
+if [ -z "${apple_dev_password}" ]; then
+  echo "ERROR: Missing Apple developer password."
+  exit 1
+fi
+if [ -z "${framework_path}" ]; then
+  echo "ERROR: Missing framework path to sign."
+  exit 1
+fi
+if [ -z "${archive_path}" ]; then
+  echo "ERROR: Missing target archive path."
+  exit 1
+fi
+
+# Sign and notarize the framework.
+
+echo "Signing and verifying the framework at '${framework_path}'..."
+
+codesign --verify --timestamp --verbose --deep --sign "${apple_dev_app_id}" "${framework_path}"
+codesign --verify "${framework_path}"
+
+echo "Archiving and notarizing the signed framework..."
+
+ditto -ck "${framework_path}" "${archive_path}"
+xcrun notarytool submit "${archive_path}" --apple-id ${apple_dev_id} --team-id ${apple_dev_team_id} --password ${apple_dev_password} --wait
